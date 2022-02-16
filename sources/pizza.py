a8b4f9c9e44e7ffef6465b146b1d9d1dec36acda
silve <gregoire.lambla@gmail.com>
2022-02-16 10:41:07 +0100

    My new pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,7 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
-    }
+    },
+    'gringo': {'ham': 2},
+    'price': 10,
 }
